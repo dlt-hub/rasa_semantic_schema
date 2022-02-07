1563de988f2786227a3f7dd2fb4505bdaae90719
Adrian <Adrian>
2022-02-07 12:57:00 +0100

    recalculate user and minor improvements

--- models/staging/stg_event_sequence.sql
+++ models/staging/stg_event_sequence.sql
@@ -14,7 +14,7 @@ WITH events AS
 prev_actions as
   (select
     *,
-    lag("timestamp") over (partition by sender_id order by "timestamp") as previous_ts
+    lag("timestamp") over (partition by user_id order by "timestamp") as previous_ts
   from events
   ),
 sessionify as
@@ -31,7 +31,7 @@ sessionify as
           )
           then 1 else 0 end
         )
-        over (partition by sender_id order by "timestamp" rows between unbounded preceding and current row),
+        over (partition by user_id order by "timestamp" rows between unbounded preceding and current row),
       1::bigint) as session_nr
     from prev_actions
   ),
@@ -40,24 +40,24 @@ turnify as
     *,
     sum(case when event = 'user' then 1 else 0 end)
       over (
-        partition by sender_id, session_nr order by "timestamp" rows between unbounded preceding and current row
+        partition by user_id, session_nr order by "timestamp" rows between unbounded preceding and current row
       ) as interaction_nr,
     -- previous actor in the same session
-    lag(event) over (partition by sender_id order by "timestamp") as previous_actor,
+    lag(event) over (partition by user_id order by "timestamp") as previous_actor,
     -- active form in session
     NULLIF(last_value(case when event ='active_loop' then coalesce(value, '---unset') else null end ignore nulls)
-      over (partition by sender_id, session_nr order by "timestamp" rows between unbounded preceding and current row),
+      over (partition by user_id, session_nr order by "timestamp" rows between unbounded preceding and current row),
       '---unset') as active_form,
     -- active form numer in session
     sum(
         case when event = 'active_loop' and value IS NOT NULL then 1 else 0 END
-    ) OVER (PARTITION BY sender_id, session_nr ORDER BY timestamp rows between unbounded preceding and current row) as active_form_nr
+    ) OVER (PARTITION BY user_id, session_nr ORDER BY timestamp rows between unbounded preceding and current row) as active_form_nr
     --  todo: slot fill step - could tag the slot fill that is in progress
     from sessionify
   )
 select *,
- max(interaction_nr) over (partition by sender_id) - interaction_nr as reverse_interaction_nr,
- (sender_id ||  '/' ||  session_nr ||  '/'||  interaction_nr) as interaction_id,
- (sender_id ||  '/' ||  session_nr ) as session_id
+ max(interaction_nr) over (partition by user_id) - interaction_nr as reverse_interaction_nr,
+ (user_id ||  '/' ||  session_nr ||  '/'||  interaction_nr) as interaction_id,
+ (user_id ||  '/' ||  session_nr ) as session_id
 from turnify
 order by timestamp asc
